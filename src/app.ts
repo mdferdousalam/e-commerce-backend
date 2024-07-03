b808871d7a4257e957e6ccfe19b6445d331a395f
mdferdousalam <mdferdousalam1989@yahoo.com>
2024-07-03 23:19:35 +0600

    eslint and prettier re installed

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send(`Server health is good and running well`);
+  res.send(`Server health is good and running well`);
 });
 
 export default app;
