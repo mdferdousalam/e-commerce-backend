c4e0d4b1b14a9b526d17305ed50071b915e7677d
mdferdousalam <mdferdousalam1989@yahoo.com>
2024-07-04 10:15:14 +0600

    zod schema created

--- src/app/modules/product/product.schema.ts
+++ src/app/modules/product/product.schema.ts
@@ -1,1 +1,19 @@
-//product schema page
+import { z } from 'zod';
+
+export const productSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  price: z.number().positive(),
+  category: z.string(),
+  tags: z.array(z.string()),
+  variants: z.array(
+    z.object({
+      type: z.string(),
+      value: z.string(),
+    }),
+  ),
+  inventory: z.object({
+    quantity: z.number().int().nonnegative(),
+    inStock: z.boolean(),
+  }),
+});
